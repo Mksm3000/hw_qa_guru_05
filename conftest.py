c2a68efd338dcb7c5c596558da780874f7d49c8d
Maksim Zosimov <mizo-y@yandex.ru>
2024-06-03 18:08:12 +0200

    построчно работает - всё вместе нет. в чём проблема?

--- conftest.py
+++ conftest.py
@@ -1,24 +1,9 @@
 import pytest
 from selene import browser
-from selenium import webdriver
 
 
 @pytest.fixture(scope="session", autouse=True)
 def browser_management():
-    browser.config.base_url = "https://demoqa.com/automation-practice-form"
-
-    # we want to type text via JavaScript to speed up tests a bit
-    browser.config.type_by_js = True
-
-    browser.config.window_width = 1600
-    browser.config.window_height = 900
-    # browser.config.timeout = 7
-
-    driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument("--headless=new")
-
-    browser.config.driver_options = driver_options
-
+    browser.config.base_url = "https://demoqa.com/"
     yield
-
     browser.quit()
--- test_demoqa.py
+++ test_demoqa.py
@@ -1,29 +1,22 @@
-from selene import browser, have
+from selene import browser, command, have
 import os
 
 
-FIRSTNAME = "Ivan"
-LASTNAME = "Petroff"
-USEREMAIL = "ivan@petroff.com"
-USERNUMBER = "0958877666"
-DATEOFBIRTHINPUT = "12 Jun 2006"
-SUBJECTSINPUT = "English"
-UPLOADPICTURE = os.path.abspath("qa_guru.png")
-CURRENTADDRESS = "Capital city, Liberty str, 17"
-
-
 def test_registration_form():
-    browser.open("/")
+    browser.open("automation-practice-form")
 
     browser.element(".text-center").should(have.text("Practice Form"))
-    browser.element("#firstName").type(FIRSTNAME).press_enter()
-    browser.element("#lastName").type(LASTNAME).press_enter()
-    browser.element("#userEmail").type(USEREMAIL).press_enter()
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element("#userNumber").type(USERNUMBER).press_enter()
-    browser.element("#dateOfBirthInput").type(DATEOFBIRTHINPUT).press_enter()
-    browser.element("#subjectsInput").type(SUBJECTSINPUT).press_enter()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element("#uploadPicture").send_keys(UPLOADPICTURE)
-    browser.element("#currentAddress").type(CURRENTADDRESS)
-    browser.element("#submit").click()
+
+    browser.element("#firstName").type("Ivan").press_enter()
+    browser.element("#lastName").type("Petroff").press_enter()
+    browser.element("#userEmail").type("ivan@petroff.com").press_enter()
+    browser.element('[for="gender-radio-1"]').perform(command.js.click)
+    browser.element("#userNumber").type("0958877666").press_enter()
+    browser.element("#dateOfBirthInput").type("12 Jun 1986").press_enter()
+    browser.element("#subjectsInput").type("English").press_enter()
+    browser.element('[for="hobbies-checkbox-2"]').perform(command.js.click)
+    browser.element("#uploadPicture").send_keys(os.path.abspath("qa_guru.png"))
+    browser.element("#currentAddress").type("Capital city, Liberty str, 17")
+    browser.element("#react-select-3-input").type("NCR").press_enter()
+    browser.element("#react-select-4-input").type("Delhi").press_enter()
+    browser.element("#submit").perform(command.js.click)
